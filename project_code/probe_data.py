d56b714250ca3b1ee81c9702fecaa694014edd7f
Stephen Lockhart <sblockhart.zzero@gmail.com>
2026-05-17 19:00:43 -0400

    Still exploring data

--- project_code/probe_data.py
+++ project_code/probe_data.py
@@ -3,8 +3,9 @@
 Probe data from Seabird Tracking Database, demonstrating how to use movingpandas to get speed
 
 To do:
--Combine date, time fileds into a timestamp
--Convert to geopandas/movingpandas to get speed per data point, per track
+-Verify conversion to geopandas
+-What is units of speed?
+-Plot of mpd trajectory, color-coded by speed
 
 """
 # Imports
@@ -45,4 +46,36 @@ lon_array = np.array(df_lon)
 
 # Plot
 ax1 = plt.plot(lon_array,lat_array)
+plt.show()
+
+# Convert to datetime
+# Add new column, derived timestamp_datetime, initialized to 1/1/1970
+df["timestamp_datetime"] = datetime(1970,1,1)
+# Convert
+format_string = "%Y-%m-%d %H:%M:%S"
+for k in range(len(df)):
+    date_string = df.loc[k,"date_gmt"]
+    time_string = df.loc[k,"time_gmt"]
+    datetime_string = date_string + ' ' + time_string
+    df.loc[k,"timestamp_datetime"] = datetime.strptime(datetime_string,format_string)
+#endfor
+
+# Fewer columns
+df_subset = df[['bird_id','timestamp_datetime','longitude','latitude']]
+
+# Convert to geopandas
+geometry = gpd.points_from_xy(df_subset['longitude'],df_subset['latitude'])
+gdf_subset = gpd.GeoDataFrame(df_subset,geometry=geometry,crs="EPSG:4326")
+
+# Before converting to movingpandas, we need to set index to timestamp_datetime
+gdf_subset.set_index('timestamp_datetime')
+print(gdf_subset.head())
+
+# Convert to movingpandas
+traj = mpd.Trajectory(gdf_subset,this_bird_id, traj_id_col='bird_id', t='timestamp_datetime')
+traj.add_speed(overwrite=True)
+print(traj.df.describe())
+
+# Plot
+ax2 = plt.plot(traj.df['speed'])
 plt.show()
