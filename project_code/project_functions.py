189f6d86c419bce4ef876f6d2ae48229266fe9e4
Stephen Lockhart <sblockhart.zzero@gmail.com>
2026-06-06 00:28:30 -0400

    Started integrating ocean color and animal telemetry in notebook

--- project_code/project_functions.py
+++ project_code/project_functions.py
@@ -15,6 +15,8 @@ def get_gdf_with_speed(input_csv_fullpath, speed_min, speed_max):
     # Convert to datetime
     # Add new column, derived timestamp_datetime, initialized to 1/1/1970
     df["timestamp_datetime"] = datetime(1970,1,1)
+    # Add a copy of this column, to be used as an index
+    df["timestamp_datetime_index"] =  df["timestamp_datetime"] 
     # Convert
     format_string = "%Y-%m-%d %H:%M:%S"
     for k in range(len(df)):
@@ -22,17 +24,18 @@ def get_gdf_with_speed(input_csv_fullpath, speed_min, speed_max):
         time_string = df.loc[k,"time_gmt"]
         datetime_string = date_string + ' ' + time_string
         df.loc[k,"timestamp_datetime"] = datetime.strptime(datetime_string,format_string)
+        df.loc[k,"timestamp_datetime_index"] = datetime.strptime(datetime_string,format_string)
     #endfor
 
     # Fewer columns
-    df_subset = df[['bird_id','timestamp_datetime','longitude','latitude']]
+    df_subset = df[['bird_id','timestamp_datetime_index','timestamp_datetime','longitude','latitude']]
 
     # Convert to geopandas
     geometry = gpd.points_from_xy(df_subset['longitude'],df_subset['latitude'])
     gdf_subset = gpd.GeoDataFrame(df_subset,geometry=geometry,crs="EPSG:4326")
 
     # Before converting to movingpandas, we need to set index to timestamp_datetime
-    gdf_subset.set_index('timestamp_datetime')
+    gdf_subset.set_index('timestamp_datetime_index')
     #print(gdf_subset.head())
 
     # Show unique bird ids
@@ -41,7 +44,7 @@ def get_gdf_with_speed(input_csv_fullpath, speed_min, speed_max):
 
     # Convert to movingpandas trajectory collection 
     # Try trajectory collection
-    tc = mpd.TrajectoryCollection(gdf_subset, "bird_id", t='timestamp_datetime')
+    tc = mpd.TrajectoryCollection(gdf_subset, "bird_id", t='timestamp_datetime_index')
 
     # Add speed
     tc.add_speed(overwrite=True)
